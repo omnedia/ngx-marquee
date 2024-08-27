25c6ba5cd0fc6e8cb81a7a7a16214cfddd165c49
Markus <markus@block-net.de>
2024-08-27 22:18:00 +0200

    Added stuff for performance

--- src/lib/ngx-marquee.component.ts
+++ src/lib/ngx-marquee.component.ts
@@ -1,58 +1,73 @@
-import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ContentChildren, ElementRef, Input, QueryList } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import {
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChild,
+} from "@angular/core";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 
 @Component({
-  selector: 'om-marquee',
+  selector: "om-marquee",
   standalone: true,
   imports: [CommonModule],
   templateUrl: "./ngx-marquee.component.html",
   styleUrl: "./ngx-marquee.component.scss",
 })
 export class NgxMarqueeComponent implements AfterViewInit {
-  @ContentChildren('OmMarqueeContent') elementRefs?: QueryList<ElementRef<HTMLElement>>;
+  @ViewChild("OmMarquee") marqueeRef!: ElementRef<HTMLElement>;
+
+  @ContentChildren("OmMarqueeContent") elementRefs?: QueryList<
+    ElementRef<HTMLElement>
+  >;
 
   @Input("styleClass")
   styleClass?: string;
 
-  @Input('reverse')
+  @Input("reverse")
   set reverse(reverse: boolean) {
     if (reverse) {
-      this.style['--om-marquee-reverse'] = 'reverse';
+      this.style["--om-marquee-reverse"] = "reverse";
       return;
     }
 
-    this.style['--om-marquee-reverse'] = '';
+    this.style["--om-marquee-reverse"] = "";
   }
 
-  @Input('animationDuration')
+  @Input("animationDuration")
   set animationDuration(animationDuration: string) {
-    this.style['--om-marquee-animation-duration'] = animationDuration;
+    this.style["--om-marquee-animation-duration"] = animationDuration;
   }
 
-  @Input('marqueeGap')
+  @Input("marqueeGap")
   set marqueeGap(marqueeGap: string) {
-    this.style['--om-marquee-gap'] = marqueeGap;
+    this.style["--om-marquee-gap"] = marqueeGap;
   }
 
-  @Input('pauseOnHover')
+  @Input("pauseOnHover")
   set pauseOnHover(pauseOnHover: boolean) {
     if (pauseOnHover) {
-      this.style['--om-marquee-pause'] = 'paused';
+      this.style["--om-marquee-pause"] = "paused";
       return;
     }
 
-    this.style['--om-marquee-pause'] = 'running';
+    this.style["--om-marquee-pause"] = "running";
   }
 
-  @Input('vertical')
+  @Input("vertical")
   vertical = false;
 
   style: any = {};
 
   marqueeElements: SafeHtml[] = [];
 
-  constructor(private readonly sanitizer: DomSanitizer) { }
+  isInView = false;
+  private intersectionObserver?: IntersectionObserver;
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
 
   ngAfterViewInit(): void {
     if (!this.elementRefs) {
@@ -60,7 +75,20 @@ export class NgxMarqueeComponent implements AfterViewInit {
     }
 
     this.marqueeElements = this.elementRefs?.toArray().map((ref) => {
-      return this.sanitizer.bypassSecurityTrustHtml(ref.nativeElement.outerHTML)
+      return this.sanitizer.bypassSecurityTrustHtml(
+        ref.nativeElement.outerHTML
+      );
     });
+
+    this.intersectionObserver = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        if (!this.isInView) {
+          this.isInView = true;
+        }
+      } else if (this.isInView) {
+        this.isInView = false;
+      }
+    });
+    this.intersectionObserver.observe(this.marqueeRef.nativeElement);
   }
 }
